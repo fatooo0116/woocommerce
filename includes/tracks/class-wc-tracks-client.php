74df2a8093007eef0a01b640d159ee91408455f5
Paul Sealock <psealock@gmail.com>
2019-02-01 14:26:38 +1300

    Tracks: Add a PHP client

---
+++ includes/tracks/class-wc-tracks-client.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * Send Tracks events on behalf of a user.
+ *
+ * @class   WC_Tracks_Client
+ * @package WooCommerce/Classes
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Class WC_Tracks_Client.
+ */
+class WC_Tracks_Client {
+	const PIXEL           = 'https://pixel.wp.com/t.gif';
+	const BROWSER_TYPE    = 'php-agent';
+	const USER_AGENT_SLUG = 'tracks-client';
+
+	/**
+	 * Record a Tracks event
+	 *
+	 * @param  array $event Array of event properties
+	 * @return bool|WP_Error         True on success, WP_Error on failure.
+	 */
+	public static function record_event( $event ) {
+		if ( ! $event instanceof WC_Tracks_Event ) {
+			$event = new WC_Tracks_Event( $event );
+		}
+		if ( is_wp_error( $event ) ) {
+			return $event;
+		}
+
+		$pixel = $event->build_pixel_url( $event );
+
+		if ( ! $pixel ) {
+			return new WP_Error( 'invalid_pixel', 'cannot generate tracks pixel for given input', 400 );
+		}
+
+		return self::record_pixel( $pixel );
+	}
+
+	/**
+	 * Synchronously request the pixel.
+	 *
+	 * @param string $pixel pixel url and query string.
+	 * @return bool|WP_Error         True on success, WP_Error on failure.
+	 */
+	public static function record_pixel( $pixel ) {
+		// Add the Request Timestamp and URL terminator just before the HTTP request.
+		$pixel .= '&_rt=' . self::build_timestamp() . '&_=_';
+
+		$response = wp_remote_get(
+			$pixel,
+			array(
+				'blocking'    => true, // The default, but being explicit here.
+				'timeout'     => 1,
+				'redirection' => 2,
+				'httpversion' => '1.1',
+			)
+		);
+
+		if ( is_wp_error( $response ) ) {
+			return $response;
+		}
+
+		$code = wp_remote_retrieve_response_code( $response );
+
+		if ( 200 !== $code ) {
+			return new WP_Error( 'request_failed', 'Tracks pixel request failed', $code );
+		}
+
+		return true;
+	}
+
+	/**
+	 * Create a timestap representing milliseconds since 1970-01-01
+	 *
+	 * @return string A string representing a timestamp.
+	 */
+	public static function build_timestamp() {
+		$ts = round( microtime( true ) * 1000 );
+		return number_format( $ts, 0, '', '' );
+	}
+
+	/**
+	 * Grabs the user's anon id from cookies, or generates and sets a new one
+	 *
+	 * @TODO: Determine the best way to identify sites/users with/without Jetpack connection.
+	 *
+	 * @return string An anon id for the user
+	 */
+	public static function get_anon_id() {
+		static $anon_id = null;
+
+		if ( ! isset( $anon_id ) ) {
+
+			// Did the browser send us a cookie?
+			if ( isset( $_COOKIE['tk_ai'] ) && preg_match( '#^[A-Za-z0-9+/=]{24}$#', $_COOKIE['tk_ai'] ) ) {
+				$anon_id = $_COOKIE['tk_ai'];
+			} else {
+
+				$binary = '';
+
+				// Generate a new anonId and try to save it in the browser's cookies
+				// Note that base64-encoding an 18 character string generates a 24-character anon id.
+				for ( $i = 0; $i < 18; ++$i ) {
+					$binary .= chr( wp_rand( 0, 255 ) );
+				}
+
+				$anon_id = 'jetpack:' . base64_encode( $binary );
+
+				if ( ! headers_sent()
+					&& ! ( defined( 'REST_REQUEST' ) && REST_REQUEST )
+					&& ! ( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST )
+				) {
+					setcookie( 'tk_ai', $anon_id );
+				}
+			}
+		}
+
+		return $anon_id;
+	}
+}
---
+++ includes/tracks/class-wc-tracks.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * PHP Tracks Client
+ *
+ * @class   WC_Tracks
+ * @package WooCommerce/Classes
+ *
+ * Example Usage:
+ *
+```php
+	require_once( dirname( __FILE__ ) . '/libraries/tracks/class-wc-tracks.php' );
+	$result = WC_Tracks::record_event( 'wca_test_update_product', array() );
+
+	if ( is_wp_error( $result ) ) {
+		// Handle the error in your app
+	}
+```
+ */
+
+/**
+ * Class WC_Tracks
+ */
+class WC_Tracks {
+	// @TODO: Find a good prefix.
+	const PREFIX = 'wca_test_';
+	/**
+	 * Get the identity to send to tracks.
+	 *
+	 * @TODO: Determine the best way to identify sites/users with/without Jetpack connection.
+	 *
+	 * @param int $user_id User id.
+	 * @return array Identity properties.
+	 */
+	public static function get_identity( $user_id ) {
+		$has_jetpack = class_exists( 'Jetpack' ) && is_callable( 'Jetpack::is_active' ) && Jetpack::is_active();
+
+		// Meta is set, and user is still connected.  Use WPCOM ID.
+		$wpcom_id = $has_jetpack && get_user_meta( $user_id, 'jetpack_tracks_wpcom_id', true );
+		if ( $wpcom_id && Jetpack::is_user_connected( $user_id ) ) {
+			return array(
+				'_ut' => 'wpcom:user_id',
+				'_ui' => $wpcom_id,
+			);
+		}
+
+		// User is connected, but no meta is set yet.  Use WPCOM ID and set meta.
+		if ( $has_jetpack && Jetpack::is_user_connected( $user_id ) ) {
+			$wpcom_user_data = Jetpack::get_connected_user_data( $user_id );
+			add_user_meta( $user_id, 'jetpack_tracks_wpcom_id', $wpcom_user_data['ID'], true );
+
+			return array(
+				'_ut' => 'wpcom:user_id',
+				'_ui' => $wpcom_user_data['ID'],
+			);
+		}
+
+		// User isn't linked at all.  Fall back to anonymous ID.
+		$anon_id = get_user_meta( $user_id, 'jetpack_tracks_anon_id', true );
+		if ( ! $anon_id ) {
+			$anon_id = WC_Tracks_Client::get_anon_id();
+			add_user_meta( $user_id, 'jetpack_tracks_anon_id', $anon_id, false );
+		}
+
+		if ( ! isset( $_COOKIE['tk_ai'] ) && ! headers_sent() ) {
+			setcookie( 'tk_ai', $anon_id );
+		}
+
+		return array(
+			'_ut' => 'anon',
+			'_ui' => $anon_id,
+		);
+
+	}
+
+	/**
+	 * Gather blog related properties.
+	 *
+	 * @param int $user_id User id.
+	 * @return array Blog details.
+	 */
+	public static function get_blog_details( $user_id ) {
+		return array(
+			// @TODO: Add revenue/product info and url similar to wc-tracker
+			'url'       => get_option( 'siteurl' ),
+			'blog_lang' => get_user_locale( $user_id ),
+			'blog_id'   => ( class_exists( 'Jetpack' ) && Jetpack_Options::get_option( 'id' ) ) || null,
+		);
+	}
+
+	/**
+	 * Gather details from the request to the server.
+	 *
+	 * @return array Server details.
+	 */
+	public static function get_server_details() {
+		$data = array();
+
+		$data['_via_ua'] = isset( $_SERVER['HTTP_USER_AGENT'] ) ? $_SERVER['HTTP_USER_AGENT'] : '';
+		$data['_via_ip'] = isset( $_SERVER['REMOTE_ADDR'] ) ? $_SERVER['REMOTE_ADDR'] : '';
+		$data['_lg']     = isset( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '';
+		$data['_dr']     = isset( $_SERVER['HTTP_REFERER'] ) ? $_SERVER['HTTP_REFERER'] : '';
+
+		$uri         = isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : '';
+		$host        = isset( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : '';
+		$data['_dl'] = $_SERVER['REQUEST_SCHEME'] . '://' . $host . $uri;
+
+		return $data;
+	}
+
+	/**
+	 * Record an event in Tracks - this is the preferred way to record events from PHP.
+	 *
+	 * @param string $event_name The name of the event.
+	 * @param array  $properties Custom properties to send with the event.
+	 * @return bool|WP_Error true for success or WP_Error if the event pixel could not be fired.
+	 */
+	public static function record_event( $event_name, $properties = array() ) {
+		$user = wp_get_current_user();
+
+		// We don't want to track user events during unit tests/CI runs.
+		if ( $user instanceof WP_User && 'wptests_capabilities' === $user->cap_key ) {
+			return false;
+		}
+
+		/**
+		 * Don't track users who haven't opted-in to tracking or if a filter
+		 * has been applied to turn it off.
+		 */
+		if (
+			'yes' !== get_option( 'woocommerce_allow_tracking' ) &&
+			! apply_filters( 'woocommerce_apply_tracking', true )
+		) {
+			return false;
+		}
+
+		$data = array(
+			'_en' => self::PREFIX . $event_name,
+			'_ts' => WC_Tracks_Client::build_timestamp(),
+		);
+
+		$server_details = self::get_server_details();
+		$identity       = self::get_identity( $user->ID );
+		$blog_details   = self::get_blog_details( $user->ID );
+
+		$event_obj = new WC_Tracks_Event( array_merge( $data, $server_details, $identity, $blog_details, $properties ) );
+
+		if ( is_wp_error( $event_obj->error ) ) {
+			return $event_obj->error;
+		}
+
+		return $event_obj->record();
+	}
+}
+
+
